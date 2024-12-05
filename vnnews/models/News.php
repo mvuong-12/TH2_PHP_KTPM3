45c49c0a92bc937dc38372380ac6122e9e8fd080
mvuong-12 <2251172564@e.tlu.edu.vn>
2024-12-06 00:09:57 +0700

    Tao class News

--- vnnews/models/News.php
+++ vnnews/models/News.php
@@ -1,3 +1,55 @@
 <?php
-
+class News{
+    private $id;
+    private $title;
+    private $content;
+    private $image;
+    private $created_at;
+    private $category_id;
+    public function __construct($id, $title, $content, $image, $created_at, $category_id){
+        $this->id = $id;
+        $this->title = $title;
+        $this->content = $content;
+        $this->image = $image;
+        $this->created_at = $created_at;
+        $this->category_id = $category_id;
+    }
+    public function getId(){
+        return $this->id;
+    }
+    public function getTitle(){
+        return $this->title;
+    }
+    public function getContent(){
+        return $this->content;
+    }
+    public function getImage(){
+        return $this->image;
+    }
+    public function getCreatedAt(){
+        return $this->created_at;
+    }
+    public function getCategoryId(){
+        return $this->category_id;
+    }
+    public function setID($id){
+        $this->id = $id;
+    }
+    public function setTitle($title){
+        $this->title = $title;
+    }
+    public function setContent($content){
+        $this->content = $content;
+    }
+    public function setImage($image){
+        $this->image = $image;
+    }
+    public function setCreated_at($created_at){
+        $this->created_at = $created_at;
+    }
+    public function setCategoryId($category_id){
+        $this->category_id = $category_id;
+    }
+    
+}
 ?>
