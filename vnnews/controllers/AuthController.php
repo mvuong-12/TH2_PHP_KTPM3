9051d0a4ee4b8c2c3a582fe8a885f167688f725e
mvuong-12 <2251172564@e.tlu.edu.vn>
2024-12-07 17:02:57 +0700

    them list_users

--- vnnews/controllers/AuthController.php
+++ vnnews/controllers/AuthController.php
@@ -1,6 +1,12 @@
 <?php
+require_once 'services/AuthServices.php'
 class AuthController{
-    public function login(){}
+    private $authService;
+    public function __construct(){
+        $this->authService = new AuthServices();
+    public function login(){
+
+    }
     public function logout(){}
     public function register(){}
     
--- vnnews/index.php
+++ vnnews/index.php
@@ -1,4 +1,42 @@
 <?php
+require_once '../config/config.php';
+echo "<pre>";
+echo APP_ROOT.'';
+echo "</pre>";
 //Routing: Nhận vào request và phân tích request
 session_start();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang chủ tin tức</title>
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-center text-success my-3">Việt Nam News</h1>
+        <a href="views/login.php" class="btn btn-light mb-3">Đăng nhập</a>
+        
+        <?php
+        for ($i = 0; $i < 3; $i++) {
+        ?>
+            <div class="card mb-3">
+                <div class="card-header">
+                    Featured <?= $i+1?>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Tiêu đề</h5>
+                    <p class="card-text">Tóm tắt nội dung</p>
+                    <a href="#" class="btn btn-primary">Xem chi tiết</a>
+                </div>
+            </div>
+        <?php
+        }
+        ?>
+    </div>
+<script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
